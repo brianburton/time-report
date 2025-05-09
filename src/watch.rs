31be960b4f622859acc46dd2cf99369851cb1a35
Brian Burton <brian@burton-computer.com>
2025-05-09 19:18:52 -0400

    Supports 'q' and 'r' in watch mode

--- src/watch.rs
+++ src/watch.rs
@@ -1,40 +1,143 @@
 use crate::core::AppError;
-use crate::model::DateRange;
+use crate::model::{DateRange, DayEntry};
 use crate::parse;
 use crate::report;
+use crate::watch::PollOutcome::{Changed, DoNothing, LoadFailed, Reloaded};
+use crossterm::event::KeyCode;
+use crossterm::event::{Event, poll, read};
 use crossterm::{QueueableCommand, cursor, terminal};
+use im::Vector;
+use scopeguard::defer;
 use std::fs;
 use std::io::{Write, stdout};
 use std::thread;
 use std::time::{Duration, SystemTime, SystemTimeError};
 
-pub fn watch_and_report(filename: &str, dates: DateRange) -> Result<(), AppError> {
-    let sleep_time = Duration::from_millis(200);
-    let update_delay_millis = 500;
-    let mut next_update_millis: u128 = 0;
-    loop {
-        let current_file_millis = get_last_modified(filename)?;
-        if current_file_millis >= next_update_millis {
-            next_update_millis = current_file_millis + update_delay_millis;
-            let result = parse::parse_file(filename);
-            if let Ok((all_day_entries, warnings)) = result {
-                clear_screen()?;
-                warnings.iter().for_each(|w| println!("warning: {w}"));
-                let lines = report::create_report(dates, &all_day_entries)?;
-                for line in lines {
-                    println!("{}", line);
+struct Tracker<'a> {
+    filename: &'a str,
+    next_update_millis: u128,
+    poll_wait_duration: Duration,
+    update_delay_millis: u128,
+}
+
+struct LoadedFile {
+    day_entries: Vector<DayEntry>,
+    warnings: Vector<String>,
+}
+
+enum PollOutcome {
+    DoNothing,
+    Quit,
+    Reloaded(LoadedFile),
+    Changed(LoadedFile),
+    LoadFailed(AppError),
+}
+
+impl<'a> Tracker<'a> {
+    fn new(filename: &'a str) -> Tracker<'a> {
+        Tracker {
+            filename,
+            next_update_millis: 0,
+            update_delay_millis: 500,
+            poll_wait_duration: Duration::from_millis(200),
+        }
+    }
+
+    fn next_command(&mut self) -> Result<PollOutcome, AppError> {
+        let io_err =
+            |detail: &str, e: std::io::Error| AppError::from_error("next_command", detail, e);
+        if poll(self.poll_wait_duration).map_err(|e| io_err("poll", e))? {
+            match read().map_err(|e| io_err("read", e))? {
+                Event::Key(event) => match event.code {
+                    KeyCode::Char('q') => Ok(PollOutcome::Quit),
+                    KeyCode::Char('r') => {
+                        let loaded = self.load(true);
+                        match loaded {
+                            Ok(Some(loaded)) => Ok(Reloaded(loaded)),
+                            Ok(None) => Ok(DoNothing),
+                            Err(e) => Ok(LoadFailed(e)),
+                        }
+                    }
+                    _ => Ok(DoNothing),
+                },
+                Event::Resize(_, _) => {
+                    let loaded = self.load(true);
+                    match loaded {
+                        Ok(Some(loaded)) => Ok(Reloaded(loaded)),
+                        Ok(None) => Ok(DoNothing),
+                        Err(e) => Ok(LoadFailed(e)),
+                    }
                 }
-            } else {
-                clear_screen()?;
-                println!(
-                    "error reading file: filename={} error={}",
-                    filename,
-                    result.unwrap_err()
-                );
+                _ => Ok(DoNothing),
+            }
+        } else {
+            let loaded = self.load(false);
+            match loaded {
+                Ok(Some(loaded)) => Ok(Changed(loaded)),
+                Ok(None) => Ok(DoNothing),
+                Err(e) => Ok(LoadFailed(e)),
             }
         }
-        thread::sleep(sleep_time);
     }
+
+    fn load(&mut self, reload: bool) -> Result<Option<LoadedFile>, AppError> {
+        let current_file_millis = get_last_modified(self.filename)?;
+        if current_file_millis < self.next_update_millis && !reload {
+            return Ok(None);
+        }
+        self.next_update_millis = current_file_millis + self.update_delay_millis;
+        let (day_entries, warnings) = parse::parse_file(self.filename)?;
+        Ok(Some(LoadedFile {
+            day_entries,
+            warnings,
+        }))
+    }
+}
+
+pub fn watch_and_report(filename: &str, dates: DateRange) -> Result<(), AppError> {
+    let io_err =
+        |detail: &str, e: std::io::Error| AppError::from_error("watch_and_report", detail, e);
+    terminal::enable_raw_mode().map_err(|e| io_err("enable_raw_mode", e))?;
+    defer! {
+        _=terminal::disable_raw_mode();
+    }
+    let mut tracker = Tracker::new(filename);
+    loop {
+        let outcome = tracker.next_command()?;
+        match outcome {
+            PollOutcome::Quit => return Ok(()),
+            PollOutcome::DoNothing => {
+                thread::sleep(tracker.poll_wait_duration);
+            }
+            PollOutcome::Reloaded(loaded) => {
+                print_file(&loaded, dates)?;
+            }
+            PollOutcome::Changed(loaded) => {
+                print_file(&loaded, dates)?;
+            }
+            PollOutcome::LoadFailed(error) => {
+                print_error(filename, error)?;
+            }
+        }
+    }
+}
+
+fn print_error(filename: &str, error: AppError) -> Result<(), AppError> {
+    clear_screen()?;
+    println!("error reading file: filename={} error={}", filename, error);
+    Ok(())
+}
+
+fn print_file(file: &LoadedFile, dates: DateRange) -> Result<(), AppError> {
+    clear_screen()?;
+    file.warnings
+        .iter()
+        .for_each(|w| println!("warning: {w}\r"));
+    let lines = report::create_report(dates, &file.day_entries)?;
+    for line in lines {
+        println!("{}\r", line);
+    }
+    Ok(())
 }
 
 fn get_last_modified(filename: &str) -> Result<u128, AppError> {
