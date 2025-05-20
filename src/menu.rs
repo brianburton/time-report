316e3bdc26be93cc06bcebbb63d84bc42ba68bd8
Brian Burton <brian@burton-computer.com>
2025-05-20 07:44:11 -0400

    Feature: Add menu module

---
+++ src/menu.rs
@@ -0,0 +1,143 @@
+use crossterm::style::Stylize;
+use im::Vector;
+use itertools::Itertools;
+
+#[derive(Clone)]
+pub struct MenuItem<T: Clone + Copy> {
+    name: String,
+    description: String,
+    key: char,
+    value: T,
+}
+
+impl<T: Clone + Copy> MenuItem<T> {
+    pub fn new(value: T, name: &str, description: &str) -> MenuItem<T> {
+        let key = name.to_lowercase().chars().next().unwrap();
+        MenuItem {
+            name: name.to_string(),
+            description: description.to_string(),
+            key,
+            value,
+        }
+    }
+
+    fn render(&self, selected: bool) -> String {
+        if selected {
+            format!(
+                "{}{}",
+                self.name.get(..1).unwrap().yellow().bold(),
+                self.name.get(1..).unwrap_or("").dark_yellow()
+            )
+        } else {
+            format!(
+                "{}{}",
+                self.name.get(..1).unwrap().cyan().bold(),
+                self.name.get(1..).unwrap_or("").dark_blue()
+            )
+        }
+    }
+}
+
+pub struct Menu<T: Clone + Copy> {
+    items: Vector<MenuItem<T>>,
+    selected_index: usize,
+}
+
+impl<T: Clone + Copy> Menu<T> {
+    pub fn new(items: Vector<MenuItem<T>>) -> Menu<T> {
+        Menu {
+            items,
+            selected_index: 0,
+        }
+    }
+
+    pub fn find_item(&self, c: char) -> Option<T> {
+        self.items.iter().find(|i| i.key == c).map(|i| i.value)
+    }
+
+    pub fn render(&self) -> String {
+        let mut text = String::new();
+        for (i, item) in self.items.iter().enumerate() {
+            if i > 0 {
+                text += "    ";
+            }
+            text += item.render(i == self.selected_index).as_ref();
+        }
+        text
+    }
+
+    pub fn left(&mut self) {
+        if self.selected_index == 0 {
+            self.selected_index = self.items.len() - 1;
+        } else {
+            self.selected_index -= 1;
+        }
+    }
+
+    pub fn right(&mut self) {
+        self.selected_index += 1;
+        if self.selected_index >= self.items.len() {
+            self.selected_index = 0
+        }
+    }
+
+    pub fn description(&self) -> &str {
+        self.items[self.selected_index].description.as_str()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use im::vector;
+
+    #[derive(Copy, Clone)]
+    enum MenuValue {
+        Append,
+        Reload,
+        Quit,
+    }
+
+    #[test]
+    fn test_menu_item() {
+        let item = MenuItem::new(MenuValue::Append, "Append", "Add current date to the file.");
+        assert_eq!(item.name, "Append");
+        assert_eq!(item.description, "Add current date to the file.");
+        assert_eq!(item.key, 'a');
+        assert_eq!(
+            item.render(true),
+            format!("{}{}", "A".yellow().bold(), "ppend".dark_yellow()),
+        );
+        assert_eq!(
+            item.render(false),
+            format!("{}{}", "A".cyan().bold(), "ppend".dark_blue()),
+        );
+    }
+
+    #[test]
+    fn test_menu() {
+        let menu_items = vector!(
+            MenuItem::new(MenuValue::Append, "Append", "Add current date to the file."),
+            MenuItem::new(MenuValue::Reload, "Reload", "Force reload of file."),
+            MenuItem::new(MenuValue::Quit, "Quit", "Quit the program.")
+        );
+        let mut menu = Menu::new(menu_items.clone());
+        for i in 0..menu_items.len() {
+            assert_eq!(menu.selected_index, i);
+            assert_eq!(
+                menu.render(),
+                format!(
+                    "{}    {}    {}",
+                    menu_items[0].render(menu.selected_index == 0),
+                    menu_items[1].render(menu.selected_index == 1),
+                    menu_items[2].render(menu.selected_index == 2)
+                )
+            );
+            menu.right();
+        }
+        for i in (0..menu_items.len()).rev() {
+            menu.left();
+            assert_eq!(menu.selected_index, i);
+        }
+    }
+}
